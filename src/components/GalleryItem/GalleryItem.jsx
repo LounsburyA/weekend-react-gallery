b94fc17d05880ab82d142f6016353fe853fe9a62
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-26 19:09:31 -0500

    began handling swap photo and description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function GalleryItem({ photo, updateItem}) {
-  // const [count, setCount] = useState(0);
+function GalleryItem({ photo, updateItem }) {
+  const [showDescription, setShowDescription] = useState(false);
   const handleLike = () => {
-   
-   
     console.log('like like like');
-   updateItem(photo);
-  
+    updateItem(photo);
+  }
 
-}
+  const handleRotate = () =>{
+
+
+  }
   return (
     <div className='photoItem'>
-    
+<div> </div>
       <img className="photos" src={photo.path} />
       <p> {photo.likes} People liked this</p>
-      <button onClick={handleLike}>Like</button> 
-   
-   
+      <button onClick={handleLike}>Like</button>
+
+
     </div>
   )
 }
