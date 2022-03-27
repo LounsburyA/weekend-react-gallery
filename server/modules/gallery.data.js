7f0cfa1622447e411133b597dbaabdd08fc09c4c
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-27 10:21:03 -0500

    added more photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -6,7 +6,9 @@ const galleryItems = [
     { id: 5, path: 'images/frosty.jpg', description: 'Frosty path on Falconia Ridge.', likes: 0 },
     { id: 6, path: 'images/start.jpg', description: 'Photo of a me starting my thru hike.', likes: 0 },
     { id: 7, path: 'images/splorin.jpg', description: 'Photo of a me exploring canadian islands.', likes: 0 },
-    { id: 8, path: 'images/finished.jpg', description: 'Photo of a me finishing my thru hike.', likes: 0 }
+    { id: 8, path: 'images/finished.jpg', description: 'Photo of a me finishing my thru hike.', likes: 0 },
+    { id: 9, path: 'images/morelkate.jpg', description: 'Kate found a morel!.', likes: 0 },
+    { id: 10, path: 'images/winterkate.jpg', description: 'Kate has a big winter pack.', likes: 0 }
 ];
 
 module.exports = galleryItems;
